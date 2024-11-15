249f7ee7bbd83270cd1a235e1f78e47e073f9618
jaepro <https://github.com/jokangmin/Mini_project2.git>
2024-11-15 14:35:20 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h1>Spring Boot + React + NCP</h1>
+        <h3>카운트 : 1</h3>
+         <img src="./image/망그러진 곰.png" alt="귀여운 곰" />  {/* 얘가 말하는 현재 위치는 index.html이다. 착각하면 안됨!!!! */}
+    </div>
+  );
+};
+
+export default App;
